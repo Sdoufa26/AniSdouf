fa79d20783480016457f41c20c535c4c26205462
Youssef Sadfi <youssef.sadfi2020@gmail.com>
2026-03-29 17:02:36 +0000

    Initial commit

---
+++ AniSdoufBack/src/test/java/org/example/anisdoufback/AniSdoufBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.anisdoufback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniSdoufBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
