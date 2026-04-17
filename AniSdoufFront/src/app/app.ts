9da52c991d2b5fb4a35f4f353e392bf01f60fccb
Youssef Sadfi <youssef.sadfi2020@gmail.com>
2026-04-17 17:53:53 +0000

    Ajout ping pour le back

--- AniSdoufFront/src/app/app.ts
+++ AniSdoufFront/src/app/app.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
 import { inject as injectAnalytics } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { HttpClient } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +15,16 @@ import { injectSpeedInsights } from '@vercel/speed-insights';
 export class App implements OnInit {
   protected readonly title = signal('AniSdoufFront');
   public authService = inject(AuthService);
+  private http = inject(HttpClient)
 
   ngOnInit() {
     injectAnalytics();
     injectSpeedInsights();
+
+    this.http.get('https://anisdouf.onrender.com', { responseType: 'text' })
+      .pipe(
+        catchError(() => of(null))
+      )
+      .subscribe();
   }
 }
